76b081a6fd52770b2643c14292e0eb16162fe9d1
chizz <bang627@skku.edu>
2021-02-22 17:36:33 +0900

    Added views to create/edit the posts inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name = 'post_list'),
-    path('post/<int:pk>/', views.post_detail, name = 'post_detail')
+    path('', views.post_list, name = 'post_list'),# 실 url , 참조할 view 함수, 참조되어지는 이름
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
+    path('post/new', views.post_new, name = 'post_new'),
+    path('post/edit/<int:pk>/',views.post_edit, name = "post_edit")
 ]
